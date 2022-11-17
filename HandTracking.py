f1e96b48b609733faa7199dad98c9efb3eb0fdda
fmcfall <60740193+fmcfall@users.noreply.github.com>
2022-11-17 15:21:31 +0000

    Added robot movement

--- HandTracking.py
+++ HandTracking.py
@@ -1,15 +1,32 @@
 import cv2 as cv
+import numpy as np
 import mediapipe as mp
 import time
+from sensapex import UMP
 
+# webcam
 video = cv.VideoCapture(0)
+# robot
+#ump = UMP.get_ump()
+#dev_ids = ump.list_devices()
+#manipulator = ump.get_device(1)
 
 mp_hands = mp.solutions.hands
-hands = mp_hands.Hands()
+hands = mp_hands.Hands(max_num_hands=1) # less hands = higher fps
 mp_draw = mp.solutions.drawing_utils
 
 previous_time = 0
 current_time = 0
+
+hand_numbers = [8] # index finger range from 5-8
+
+# finger position
+previous_pos = [0.5, 0.5, 0]
+current_pos = [0.5, 0.5, 0]
+
+# robot position (calibrated 0)
+robot_pos = np.array([9999.8818359375, 9999.986328125, 9999.9501953125])
+robot_pos_new = np.array([9999.8818359375, 9999.986328125, 9999.9501953125])
 
 # Run webcam
 while True:
@@ -26,9 +43,31 @@ while True:
         for hand in results.multi_hand_landmarks:
             for id, landmark in enumerate(hand.landmark):
                 landmark_h, landmark_w = int(landmark.y * image_height), int(landmark.x * image_width)
-                if id == 8 or id == 7 or id == 6 or id == 5: # 8 is index finger
-                    cv.circle(img, (landmark_w, landmark_h), 5, (255, 0, 255), cv.FILLED)
-            # Draw hand onto image (img, points, connections)
+                # according to https://google.github.io/mediapipe/solutions/hands.html, z uses similar scale to x
+                landmark_d = int(landmark.z * image_width)
+
+                if id in hand_numbers:
+                    id_norm_pos = [landmark.x, landmark.y, landmark.z]
+                    id_pos = (landmark_w, landmark_h, landmark_d)
+
+                    # draw circle on finger
+                    cv.circle(img, id_pos[:2], 5, (255, 0, 255), cv.FILLED)
+                    #print(id, id_norm_pos)
+
+                    # calcualate normalised position difference
+                    current_pos = id_norm_pos
+                    pos_diff = np.subtract(current_pos, previous_pos)
+                    previous_pos = current_pos
+                    #print(pos_diff)
+
+                    # move robot
+                    np.multiply(robot_pos[:3], (np.subtract(1, pos_diff)), robot_pos_new)
+                    #manipulator.goto_pos(robot_new_pos, speed=1500)
+                    print(robot_pos)
+                    robot_pos = robot_pos_new
+            
+
+            # Draw entire hand onto image (img, points, connections)
             # mp_draw.draw_landmarks(img, hand)
 
 
